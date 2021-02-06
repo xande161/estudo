3930d9be8df2b6491da69ca44834a17226b3740f
Alexandre Dutra <alexandredutrapinheiro@hotmail.com>
2021-02-06 16:23:35 -0400

    criando dado no banco

--- src/main/java/com/alexandre/estudo/EstudoApplication.java
+++ src/main/java/com/alexandre/estudo/EstudoApplication.java
@@ -1,13 +1,32 @@
 package com.alexandre.estudo;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.alexandre.estudo.domain.Categoria;
+import com.alexandre.estudo.repositories.CategoriaRepository;
+import com.sun.xml.bind.v2.runtime.unmarshaller.XsiNilLoader.Array;
+
 @SpringBootApplication
-public class EstudoApplication {
+public class EstudoApplication implements CommandLineRunner{
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(EstudoApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null, "Informatica");
+		Categoria cat2 = new Categoria(null, "Escritorio");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 	}
 
 }
